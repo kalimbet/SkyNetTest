d194c73904bf1678abb70bfae4d1e4237b0a2534
Viktor Surzhko <wiktor_vitos@mail.ru>
2019-05-09 19:44:33 +0300

    Add files via upload

--- SkyNet/src/com/company/Main.java
+++ SkyNet/src/com/company/Main.java
@@ -1,13 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
 	    Worker jakub = new Worker("Viktor", "Surzhko");
-	    jakub.addHour(8,2);
-        jakub.calculateRating();
 
-	    System.out.println(jakub.raing);
-
+        for(int i = 0; i < 2; i++){
+            int days = i + 1;
+            System.out.println("Day " + days);
+            System.out.println("    Write work hours: ");
+            int work = input.nextInt();
+            System.out.println("    Write rest hours: ");
+            int rest = input.nextInt();
+            System.out.println("    Completed the task?");
+            byte completed = input.nextByte();
+            System.out.println("____________________");
+            jakub.reportOfDay(work, rest, completed);
+            jakub.calculateDayRating();
+            jakub.week();
+        }
+	    System.out.println();
+        jakub.calculateWeekRating();
+        jakub.showDaysRating();
+        jakub.showWeekRating();
     }
 }
--- SkyNet/src/com/company/Worker.java
+++ SkyNet/src/com/company/Worker.java
@@ -1,21 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Worker {
     String name;
     String surname;
-    int timeOnWork = 0;
-    int timeOnRest = 0;
 
-    int raing = 0;
-    boolean finishTask = false;
+    private int timeOnWork = 0, timeOnRest = 0;
+    private int rating = 0, weekRating = 0;
+    private int days;
+    private boolean finishTask = false;
+    ArrayList<Integer> weekListRating = new ArrayList<>();
+
 
     Worker(String name, String surname){
         this.name = name;
         this.surname = surname;
+
     }
 
 
-    public void addHour(int hourOfWork, int hourOfRest){
+    public void reportOfDay(int hourOfWork, int hourOfRest, byte completed){
         int check = hourOfRest + hourOfWork;
         if(check <= 24){
             for(int i = 0; i < hourOfWork; i++){
@@ -26,27 +31,73 @@ public class Worker {
                 addOneHourRest();
             }
         }
+
+        if(completed == 1){
+            finishTask = true;
+        }else if(completed == 0){
+            finishTask = false;
+        }
     }
 
+    public void week(){
+        weekListRating.add(rating);
+        rating = 0;
+        timeOnRest = 0;
+        timeOnWork = 0;
+    }
 
-    public void calculateRating(){
+    public void showDaysRating(){
+        for(int i = 0; i < weekListRating.size(); i++){
+            days = i + 1;
+            System.out.println("Day " + days);
+            System.out.println("    Rating : " + weekListRating.get(i));
+            System.out.println("    Conclusion : " + conclusionOfDay());
+            System.out.println();
+        }
+    }
+
+    public void showWeekRating(){
+        System.out.println("Week rating : " + weekRating);
+        System.out.println("    Conclusion: " + conclusionOfWeek());
+    }
+
+    public void calculateDayRating(){
         if(timeOnWork > 6){
-            raing += 3;
+            rating += 3;
         }else{
-            raing += 1;
+            rating += 1;
         }
 
         if(timeOnRest < 5){
-            raing += 3;
+            rating += 3;
         }else{
-            raing += 1;
+            rating += 1;
         }
 
         if(finishTask == true){
-            raing += 4;
+            rating += 4;
         }else {
-            raing += 1;
+            rating += 1;
         }
+    }
+
+    public int calculateWeekRating(){
+        for(int i = 0; i < weekListRating.size(); i++){
+            weekRating += weekListRating.get(i);
+        }
+        return weekRating;
+    }
+
+    public String conclusionOfDay(){
+        if(rating >= 5 && rating <= 7) return "Acceptable result";
+        if(rating >= 8) return "Excellent result";
+        return "Bad result";
+    }
+
+    public String conclusionOfWeek(){
+        if(weekRating >= 25 && weekRating<= 40) return "Acceptable result";
+        if(weekRating > 40) return "Excellent result";
+        return "Bad reuslt";
     }
 
     private void addOneHourWork(){
